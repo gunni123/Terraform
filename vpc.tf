b8305f0ac965e9c5de0fe057840565555a93deee
gunni123 <gunavardhan.padileti@gmail.com>
2019-12-18 14:44:23 +0530

    EC2 creation

--- vpc.tf
+++ vpc.tf
@@ -5,26 +5,10 @@ resource "aws_vpc" "my_vpc" {
     Name = "Terraform"
   }
 }
-data "aws_ami" "ubuntu" {
-    most_recent = true
-
-    filter {
-      name   = "name"
-      values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-    }
-
-    filter {
-      name   = "virtualization-type"
-      values = ["hvm"]
-    }
-
-    owners = ["633373245219"] # Canonical
-  }
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "Guna" {
+  ami           = "ami-408c7f28"
   instance_type = "t2.micro"
-
+  
   tags = {
       Name = "Terraform-EC2"
   }
